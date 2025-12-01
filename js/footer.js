8a0db7af0f401161f0f8d194bc273c79dacd2d6b
SilkyRoad-marketplace <andytanth82@gmail.com>
2025-12-02 04:22:12 +0800

    Delete js directory

--- js/footer.js
+++
@@ -1,9 +0,0 @@
-export async function loadFooter() {
-  const root = document.getElementById("footer-root");
-  if (!root) return;
-  const res = await fetch("/partials/footer.html");
-  const html = await res.text();
-  root.innerHTML = html;
-}
-
-loadFooter();
